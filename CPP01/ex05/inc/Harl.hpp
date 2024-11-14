2860c548f7e078a0362cf6fa7aa1a467441ed015
Ramzy Zoldik <rabouzia@student.42.fr>
2024-11-15 00:15:21 +0100

    ex05 ex06 almost done

--- CPP01/ex05/inc/Harl.hpp
+++ CPP01/ex05/inc/Harl.hpp
@@ -1,12 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.hpp                                           :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:48 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:05:49 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/11/15 00:06:57 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug(void);
+        void info(void);
+        void warning (void);
+        void error(void);
+    public:
+        Harl();
+        void complain(std::string level);
+        ~Harl();
+    
+}
+
+#endif
