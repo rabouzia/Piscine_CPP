11dd2077f39f4c7aa951328a0075357be18e2066
Ramzy Zoldik <rabouzia@student.42.fr>
2024-12-05 04:04:21 +0100

    ex00 ex01 of cpp7 finished

--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 17:06:03 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:06:04 by rabouzia         ###   ########.fr       */
+/*   Created: 2024/12/05 03:07:38 by ramzerk           #+#    #+#             */
+/*   Updated: 2024/12/05 03:07:40 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "iter.hpp"
+
+int main() {
+	int arr1[] = {1, 2, 3, 4, 5};
+	iter(arr1, 5, printer<int>);
+	std::cout << std::endl;
+
+	double arr2[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	iter(arr2, 5, printer<double>);
+	std::cout << std::endl;
+
+	char arr3[] = {'a', 'b', 'c', 'd', 'e'};
+	iter(arr3, 5, printer<char>);
+	std::cout << std::endl;
+
+}
--- CPP07/ex02/src/Array.hpp
+++ CPP07/ex02/src/Array.hpp
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:48 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:05:49 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/12/05 03:16:49 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+
+template <class T> class Array{
+	
+	private :
+
+	public :
+		
+};
+
+
+#endif
