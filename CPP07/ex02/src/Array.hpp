91e754fff841d01e209ee6f3a2c98ee24da385bf
Ramzy Zoldik <rabouzia@student.42.fr>
2024-12-05 13:54:42 +0100

    cpp7 almost finished

--- CPP07/ex02/src/Array.hpp
+++ CPP07/ex02/src/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:48 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/12/05 03:16:49 by ramzerk          ###   ########.fr       */
+/*   Updated: 2024/12/05 13:19:43 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,18 @@
 template <class T> class Array{
 	
 	private :
-
+		T* _tab;
+		unsigned int _len;
+			
 	public :
-		
+		Array();
+		Array(const unsigned int &len);
+		Array(const Array &copy);
+		~Array();
+		Array<T> &operator=(const Array &other);
+		T &operator [](const int &index);
+		int size() const;
+				
 };
 
 
