1bec0b934992fceb4e409e9400c3ed70673f91d0
Ramzy Zoldik <rabouzia@student.42.fr>
2024-12-14 14:30:31 +0100

    strating cpp8

--- CPP08/ex01/inc/Span.hpp
+++ CPP08/ex01/inc/Span.hpp
@@ -1,12 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.hpp                                           :+:      :+:    :+:   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:48 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:05:49 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/12/14 10:29:04 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+# include <set>
+
+
+class Span{
+	
+	private:
+		
+	
+	public:
+		Span();
+		Span(const Span &copy);
+		Span operator=(const Span &src);
+		~Span();
+		
+
+		
+		int shortestSpan();
+		int longestSpan();
+		
+}
+
+#endif
--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -3,10 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:06:03 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:06:04 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/12/14 10:23:45 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+int main() {
+	srand(time(NULL));
+
+	try {
+		std::cout << "==========================================" << std::endl;
+		std::cout << "              Big Span Test               " << std::endl;
+		std::cout << "==========================================" << std::endl;
+		Span span(15000);
+
+		for (unsigned int i = 0; i < span.max_size(); i++)
+			span.addNumber(rand() % 1000000);
+	
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;	
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "==========================================" << std::endl;
+		std::cout << "              Fill Span Test              " << std::endl;
+		std::cout << "==========================================" << std::endl;
+
+		Span fill_span(10);
+		std::vector<int> vec;
+		vec.push_back(27);
+		vec.push_back(42);
+		vec.push_back(69);
+		fill_span.addNumbers(vec.begin(), vec.end());
+		std::cout << "Fill Span Max Size: " << fill_span.max_size() << " Fill Span Size: " << fill_span.size() << std::endl;
+		std::cout << "Longest span: " << fill_span.longestSpan() << std::endl;
+		std::cout << "Shortest span: " << fill_span.shortestSpan() << std::endl;
+		std::cout << "Adding 8" << std::endl;
+		fill_span.addNumber(8);
+		std::cout << "Fill Span Max Size: " << fill_span.max_size() << " Fill Span Size: " << fill_span.size() << std::endl;
+		std::cout << "Longest span: " << fill_span.longestSpan() << std::endl;
+		std::cout << "Shortest span: " << fill_span.shortestSpan() << std::endl;
+		std::cout << "Adding 8 again" << std::endl;
+		fill_span.addNumber(8);
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "==========================================" << std::endl;
+	std::cout << "               Subject test               " << std::endl;
+	std::cout << "==========================================" << std::endl;
+
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	return 0;
+}
