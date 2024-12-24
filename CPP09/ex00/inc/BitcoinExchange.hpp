b4e80181c672e9b1fd400b0c136a06c3980bd177
Ramzy Zoldik <rabouzia@student.42.fr>
2024-12-24 15:28:04 +0100

    cpp9 almost finishe

--- CPP09/ex00/inc/BitcoinExchange.hpp
+++ CPP09/ex00/inc/BitcoinExchange.hpp
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.hpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:48 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/11/11 17:05:49 by rabouzia         ###   ########.fr       */
+/*   Updated: 2024/12/20 20:13:35 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <map>
+#include <iostream> 
+
+class BitcoinExchange{
+	
+	private:
+			std::map<std::string, double> _map;
+	public:
+		BitcoinExchange();
+		BitcoinExchange(BitcoinExchange const &copy);
+		BitcoinExchange & operator =(BitcoinExchange const & other);
+		~BitcoinExchange();
+
+		bool ProcessFile(std::string file);
+			
+}
